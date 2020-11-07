c0e340578a1d9947e009eed6d15efb02b0c3aa5c
duweifeng <duweifeng@comisys.net>
2020-11-07 16:29:14 +0800

    leetcode-2

--- src/main/java/com/noah/leetcode/code/One.java
+++ src/main/java/com/noah/leetcode/code/One.java
@@ -28,6 +28,8 @@ public class One {
     }
 
     public static int[] twoSum(int[] nums, int target) {
+        
+        //
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
